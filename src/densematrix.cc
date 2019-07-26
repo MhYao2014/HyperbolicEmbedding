7ba8649384ed2c542ff94102431b4f831912a305
MhYao2014 <yaopeizhang1@yeah.net>
2019-07-26 23:08:23 +0800

    删除不必要的文件夹

---
+++ src/densematrix.cc
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "densematrix.h"
+
+#include <exception>
+#include <random>
+#include <stdexcept>
+#include <utility>
+#include <math.h>
+#include <iostream>
+#include "utils.h"
+#include "vector.h"
+
+namespace fasttext {
+
+DenseMatrix::DenseMatrix() : DenseMatrix(0, 0) {}
+
+DenseMatrix::DenseMatrix(int64_t m, int64_t n) : Matrix(m, n), data_(m * n) {}
+
+DenseMatrix::DenseMatrix(DenseMatrix&& other) noexcept
+    : Matrix(other.m_, other.n_), data_(std::move(other.data_)) {}
+
+void DenseMatrix::zero() {
+  std::fill(data_.begin(), data_.end(), 0.0);
+}
+
+void DenseMatrix::uniform(real a) {
+  std::minstd_rand rng(1);
+  std::uniform_real_distribution<> uniform(-a, a);
+  for (int64_t i = 0; i < (m_ * n_); i++) {
+    data_[i] = uniform(rng);
+  }
+}
+
+void DenseMatrix::multiplyRow(const Vector& nums, int64_t ib, int64_t ie) {
+  if (ie == -1) {
+    ie = m_;
+  }
+  assert(ie <= nums.size());
+  for (auto i = ib; i < ie; i++) {
+    real n = nums[i - ib];
+    if (n != 0) {
+      for (auto j = 0; j < n_; j++) {
+        at(i, j) *= n;
+      }
+    }
+  }
+}
+
+void DenseMatrix::divideRow(const Vector& denoms, int64_t ib, int64_t ie) {
+  if (ie == -1) {
+    ie = m_;
+  }
+  assert(ie <= denoms.size());
+  for (auto i = ib; i < ie; i++) {
+    real n = denoms[i - ib];
+    if (n != 0) {
+      for (auto j = 0; j < n_; j++) {
+        at(i, j) /= n;
+      }
+    }
+  }
+}
+
+real DenseMatrix::l2NormRow(int64_t i) const {
+  auto norm = 0.0;
+  for (auto j = 0; j < n_; j++) {
+    norm += at(i, j) * at(i, j);
+  }
+  if (std::isnan(norm)) {
+    throw std::runtime_error("Encountered NaN.");
+  }
+  return std::sqrt(norm);
+}
+
+void DenseMatrix::l2NormRow(Vector& norms) const {
+  assert(norms.size() == m_);
+  for (auto i = 0; i < m_; i++) {
+    norms[i] = l2NormRow(i);
+  }
+}
+
+real DenseMatrix::dotRow(const Vector& vec, int64_t i) const {
+  assert(i >= 0);
+  assert(i < m_);
+  assert(vec.size() == n_);
+  real d = 0.0;
+  for (int64_t j = 0; j < n_; j++) {
+    d += at(i, j) * vec[j];
+  }
+  if (std::isnan(d)) {
+    throw std::runtime_error("Encountered NaN.");
+  }
+  return d;
+}
+
+void DenseMatrix::addVectorToRow(const Vector& vec, int64_t i, real a) {
+  assert(i >= 0);
+  assert(i < m_);
+  assert(vec.size() == n_);
+  for (int64_t j = 0; j < n_; j++) {
+    data_[i * n_ + j] += a * vec[j];
+  }
+}
+
+real DenseMatrix::lorentzNorm(Vector& v) {
+    real result = -v[0] * v[0];
+    for (int64_t i = 1; i < v.size(); i++) {
+        result = result + v[i] * v[i];
+    }
+//    std::cerr << "\r" << result << "\r" << std::endl;
+//    result = fmax(result, 0.01);
+    assert( result > 0);
+    return std::sqrt(result);
+}
+
+void DenseMatrix::expMapToRow(fasttext::Vector & grad, int64_t i) {
+  assert(i >= 0);
+  assert(i < m_);
+  assert(grad.size() - 1 == n_);
+  real gradLorentzNorm = lorentzNorm(grad);
+  real coshNorm = cosh(gradLorentzNorm);
+  real sinhNorm = sinh(gradLorentzNorm);
+  // 把第一维度空开，跳过。
+  for (int64_t j = 0; j < n_; j++) {
+    data_[i * n_ + j] = data_[i * n_ + j] * coshNorm + sinhNorm / gradLorentzNorm * grad[j + 1];
+  }
+}
+
+void DenseMatrix::addRowToVector(Vector& x, int32_t i) const {
+  assert(i >= 0);
+  assert(i < this->size(0));
+  assert(x.size() == this->size(1));
+  for (int64_t j = 0; j < this->size(1); j++) {
+    x[j] += at(i, j);
+  }
+}
+
+void DenseMatrix::addRowToVector(Vector& x, int32_t i, real a) const {
+  assert(i >= 0);
+  assert(i < this->size(0));
+  assert(x.size() == this->size(1));
+  for (int64_t j = 0; j < this->size(1); j++) {
+    x[j] += a * at(i, j);
+  }
+}
+
+void DenseMatrix::save(std::ostream& out) const {
+  out.write((char*)&m_, sizeof(int64_t));
+  out.write((char*)&n_, sizeof(int64_t));
+  out.write((char*)data_.data(), m_ * n_ * sizeof(real));
+}
+
+void DenseMatrix::load(std::istream& in) {
+  in.read((char*)&m_, sizeof(int64_t));
+  in.read((char*)&n_, sizeof(int64_t));
+  data_ = std::vector<real>(m_ * n_);
+  in.read((char*)data_.data(), m_ * n_ * sizeof(real));
+}
+
+void DenseMatrix::dump(std::ostream& out) const {
+  out << m_ << " " << n_ << std::endl;
+  for (int64_t i = 0; i < m_; i++) {
+    for (int64_t j = 0; j < n_; j++) {
+      if (j > 0) {
+        out << " ";
+      }
+      out << at(i, j);
+    }
+    out << std::endl;
+  }
+};
+
+} // namespace fasttext
